cba0354c8765b84cd0993ebef6fcf75565a9ae3e
Tomokazu Wakabayashi <187154953+tomo-waka@users.noreply.github.com>
2024-12-20 14:12:04 +0900

    feat: update version to v0.1.8

--- packages/gent/generated/packageEnv.ts
+++ packages/gent/generated/packageEnv.ts
@@ -2,6 +2,6 @@
 export const packageEnv = {
     fullName: "@gent-js/gent",
     name: "gent",
-    version: "0.1.7",
+    version: "0.1.8",
     description: "template-based data generator."
 } as const;
