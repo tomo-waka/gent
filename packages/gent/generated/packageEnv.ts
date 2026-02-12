d45583a7c629f9358f5152ac06aa71710f72f521
wakabayashi <187154953+tomo-waka@users.noreply.github.com>
2026-02-12 17:46:42 +0900

    Bump version of @gent-js/gent and @gent-js/gent-sea to 0.1.18 in package.json and package-lock.json

--- packages/gent/generated/packageEnv.ts
+++ packages/gent/generated/packageEnv.ts
@@ -2,6 +2,6 @@
 export const packageEnv = {
     fullName: "@gent-js/gent",
     name: "gent",
-    version: "0.1.17",
+    version: "0.1.18",
     description: "template-based data generator."
 } as const;
